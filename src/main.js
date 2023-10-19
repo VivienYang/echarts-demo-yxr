88a4ab494c8a243ffa7d0f91a4e14e52ecfb45ec
vivien_yang <1740768148@qq.com>
2023-10-19 15:03:25 +0800

    echarts 移动端适配

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@
 let echarts = require('echarts');
 console.log(111);
 console.log(echarts);
-
-// 基于准备好的dom，初始化echarts实例
 const main = document.getElementById('main');
+let clientWidth = document.documentElement.clientWidth;
+main.style.width = `${clientWidth}px`;
+main.style.height = `${clientWidth * 1.2}px`;
+
+// 基于准备好的dom，初始化echarts实例;
 let myChart = echarts.init(main, 'default');
 
 // 指定图表的配置项和数据
@@ -21,30 +24,54 @@ function createYdata() {
 }
 let xData = [createXdata(), createXdata()];
 let valData = [createYdata(), createYdata()];
-const option = {
-  title: {
-    text: 'echarts折线图',
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption({
+  baseOption: {
+    title: {
+      text: 'echarts折线图',
+    },
+    legend: {
+      data: ['2023年1月出勤天数'],
+    },
+    xAxis: {
+      type: 'category',
+      data: xData,
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        lineStyle: {
+          color: 'red',
+        },
+        itemStyle: {
+          borderWidth: 300,
+        },
+        name: '2023年1月出勤天数',
+        data: valData,
+        type: 'line',
+      },
+    ],
   },
-  legend: {
-    data: ['2023年1月出勤天数'],
-  },
-  xAxis: {
-    type: 'category',
-    data: xData,
-  },
-  yAxis: {
-    type: 'value',
-  },
-  series: [
+  media: [
     {
-      name: '2023年1月出勤天数',
-      data: valData,
-      type: 'line',
+      query: {
+        maxWidth: 500,
+      },
+      option: {
+        series: [
+          {
+            lineStyle: {
+              color: 'blue',
+            },
+          },
+        ],
+      },
     },
   ],
-};
-// 使用刚指定的配置项和数据显示图表。
-myChart.setOption(option);
+});
 let isLoading = false;
 document.querySelector('#loadMore').addEventListener('click', () => {
   if (isLoading) return;
@@ -63,10 +90,10 @@ document.querySelector('#loadMore').addEventListener('click', () => {
     });
     myChart.hideLoading();
     isLoading = false;
-  }, 2000);
+  }, 500);
 });
-myChart.on('click',(e)=>{
-  console.log(e.name)
-  console.log(e.data)
-  window.open(`https://www.baidu.com?time=${e.name}&val=${e.data}`)
-})
+myChart.on('click', (e) => {
+  console.log(e.name);
+  console.log(e.data);
+  // window.open(`https://www.baidu.com?time=${e.name}&val=${e.data}`);
+});
